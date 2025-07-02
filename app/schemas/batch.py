4a15e8963c3cbc3ebe47819eec2265c80d76a6cf
kostin <RocketMenace>
2025-07-03 00:04:52 +0300

    build:
    Start setup schemas.

---
+++ app/schemas/batch.py
@@ -0,0 +1,7 @@
+from pydantic import BaseModel
+
+
+class Batch(BaseModel):
+    pass
+
+
---
+++ app/schemas/order_line.py
@@ -0,0 +1,14 @@
+from pydantic import BaseModel, ConfigDict, Field
+
+
+class OrderLine(BaseModel):
+
+    model_config = ConfigDict(frozen=True)
+    order_id: int = Field(default=..., title="order_id")
+    sku: str = Field(default=..., title="sku")
+    qty: int = Field(default=..., title="qty")
+
+
+
+
+
